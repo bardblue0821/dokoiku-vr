7b1fc78b7a967ac953dc75a038eaefd1375f7e26
baldblue0821 <bardblue0821@gmail.com>
2023-11-28 19:18:10 +0900

    Modify column 'image' as nullable

--- database/migrations/2023_11_27_152437_add_image_to_posts_table.php
+++ database/migrations/2023_11_27_152437_add_image_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->text('image');
+            $table->text('image')->nullable();
         });
     }
 
