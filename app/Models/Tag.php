9ed27188f4480ee8b91a3e4f48a126265a073960
baldblue0821 <bardblue0821@gmail.com>
2023-12-05 14:19:23 +0900

    Created tag table and seeded

---
+++ app/Models/Tag.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tags';
+    protected $primarykey = 'tag_id';  // needed if primary key name != 'id'
+}
