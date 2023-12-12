6560fd257b74865634477b66a1b7dc00b6ce00cd
baldblue0821 <bardblue0821@gmail.com>
2023-12-12 14:40:54 +0900

    Modify incorrect link in Title in each card

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -92,12 +92,12 @@
                 <!-- article - start -->
                     <div class="flex flex-col overflow-hidden rounded-lg border bg-white">
                         <a href="{{route('post.show', $post)}}" class="group relative block h-48 overflow-hidden bg-gray-100 md:h-64">
-                        <img src='{{$image->path}}' loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+                            <img src='{{$image->path}}' loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
                         </a>
 
                         <div class="flex flex-1 flex-col p-4 sm:p-6">
                             <h2 class="mb-2 text-lg font-semibold text-gray-800">
-                            <a href="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{{\Illuminate\Support\Str::limit($post->title, 100, '...')}}</a>
+                            <a href="{{route('post.show', $post)}}" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{{\Illuminate\Support\Str::limit($post->title, 100, '...')}}</a>
                         </h2>
 
                         <p class="mb-8 text-gray-500">{{\Illuminate\Support\Str::limit($post->body, 100, '...')}}</p>
