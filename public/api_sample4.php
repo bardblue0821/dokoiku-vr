88eb2676c3d672d6f0d09fbb54a3a58a325e5df9
baldblue0821 <bardblue0821@gmail.com>
2024-01-15 13:49:20 +0900

    Test to execute GET API function

---
+++ public/api_sample4.php
@@ -0,0 +1,25 @@
+<?php
+
+$url = "https://api.vrchat.cloud/api/1/worlds/wrld_7acbb0fb-0c54-4ab0-948a-34e2f924c568";
+
+//cURLセッションを初期化する
+$ch = curl_init();
+
+//URLを指定する
+curl_setopt($ch, CURLOPT_URL, $url);
+
+//クッキー情報を取得するファイルを指定する
+$cookiefile = 'cookie.txt';
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiefile);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiefile);
+
+// ユーザーエージェントを指定する
+$userAgent = "Laravel/1.0 (bardblue0821@gmail.com)"; // ここに適切なアプリケーション名やバージョン、連絡先情報を入力
+curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+
+//URLの情報を取得する
+$res = curl_exec($ch);
+
+//セッションを終了する
+curl_close($ch);
+?>
