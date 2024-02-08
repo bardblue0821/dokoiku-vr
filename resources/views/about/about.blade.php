aef4578cc9d5ab7cbc5615cfcae933629a58bc4f
baldblue0821 <bardblue0821@gmail.com>
2024-02-08 21:19:59 +0900

    Activated photo posting function #43

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -104,15 +104,15 @@
 
       <!-- feature - start -->
       <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-gray-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-teal-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
           </svg>
         </div>
 
         <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">おやすみワールド検索(予定)</h3>
-          <p class="mb-2 text-gray-500">QVペンタグ、ジョインログ、ベッド、動画プレイやタグを活用して、おやすみワールドデータベースを作ろう！</p>
+          <h3 class="mb-2 text-lg font-semibold md:text-xl">おやすみワールド検索</h3>
+          <p class="mb-2 text-gray-500">ペン、ジョインログ、ベッド、動画プレイヤーなどの情報をまとめて、おやすみワールドデータベースを作ろう！</p>
         </div>
       </div>
       <!-- feature - end -->
@@ -134,15 +134,15 @@
 
       <!-- feature - start -->
       <div class="flex gap-4 md:gap-6">
-        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-gray-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+        <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-teal-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
           </svg>
         </div>
 
         <div>
-          <h3 class="mb-2 text-lg font-semibold md:text-xl">写真投稿(予定)</h3>
-          <p class="mb-2 text-gray-500">いい写真取れた！？ぜひみんなに共有しよう！</p>
+          <h3 class="mb-2 text-lg font-semibold md:text-xl">写真投稿</h3>
+          <p class="mb-2 text-gray-500">いい写真撮れた！？みんなと共有して思い出のアルバムを作ろう！</p>
         </div>
       </div>
       <!-- feature - end -->
